bf87970c65d4ff51af5a3aae38d9e5104b0a1881
Abdulaziz <abdulazizfaactory@gmail.com>
2021-03-14 22:49:07 +0500

    Ex: 5, part 1 and 2 done.

--- webpage/music.php
+++ webpage/music.php
@@ -17,17 +17,21 @@
         <?php
         //        $q = $_REQUEST["playlist"];// Gives an error
         if (isset($_REQUEST["playlist"])) {
-            $querySongs = file("songs/{$_REQUEST["playlist"]}");
-            ?>
-            <a href="../webpage/music.php" id="return">Back</a>
-            <?php foreach ($querySongs as $file) { ?>
-                <li class="mp3item">
-                    <a href="<?= "songs/{$file}" ?>"><?= $file ?></a>
-                </li>
-            <?php } ?>
+        $querySongs = file("songs/{$_REQUEST["playlist"]}");
+        ?>
+        <a href="../webpage/music.php" id="return">Back</a>
+        <?php foreach ($querySongs as $file) {
+            if (strchr($file, '#')) {
+                continue;
+            } ?>
+            <li class="mp3item">
+                <a href="<?= "songs/{$file}" ?>"><?= $file ?></a>
+            </li>
+        <?php } ?>
     </ul>
+
     <ul id="musiclist">
-    <?php } else {
+        <?php } else {
             $songs = glob("songs/*.mp3");
             foreach ($songs as $song) {
                 $songName = basename($song);
@@ -37,15 +41,15 @@
                     <a href="<?= $song ?>"><?= $songName ?></a>
                     <?php if ($size < 1023) { ?>
                         (<?= round($size, 2) ?> b)
-                    <?php } elseif (($size >= 1048576 )) { ?>
-                    (<?= round($size / 1048576, 2) ?>mb)
+                    <?php } elseif (($size >= 1048576)) { ?>
+                        (<?= round($size / 1048576, 2) ?>mb)
                     <?php } elseif ($size >= 1024 and $size <= 1048576) { ?>
                         (<?= round($size / 1024, 2) ?>kb)
                     <?php } ?>
                 </li>
             <?php } ?>
 
-            <?php $playlists = glob("songs/*.txt");
+            <?php $playlists = glob("songs/*.m3u");
             foreach ($playlists as $playlist) {
                 $playlistName = basename($playlist);
                 ?>
@@ -55,38 +59,6 @@
             <?php }
         }
         ?>
-    </ul>
-
-        <!---->
-        <!---->
-        <!--        <li class="mp3item">-->
-        <!--            <a href="songs/Be More.mp3">Be More.mp3</a>-->
-        <!--            (5438375 b)-->
-        <!--        </li>-->
-        <!---->
-        <!--        <li class="mp3item">-->
-        <!--            <a href="songs/Drift Away.mp3">Drift Away.mp3</a>-->
-        <!--            (5724612 b)-->
-        <!--        </li>-->
-        <!---->
-        <!--        <li class="mp3item">-->
-        <!--            <a href="songs/Hello.mp3">Hello.mp3</a>-->
-        <!---->
-        <!--            (1871110 b)-->
-        <!--        </li>-->
-        <!---->
-        <!--        <li class="mp3item">-->
-        <!--            <a href="songs/Panda Sneeze.mp3">Panda Sneeze.mp3</a>-->
-        <!--            (58 b)-->
-        <!--        </li>-->
-        <!---->
-        <!--        <li class="playlistitem">-->
-        <!--            <a href="music.php?playlist=mypicks.txt">mypicks.txt</a>-->
-        <!--        </li>-->
-        <!---->
-        <!--        <li class="playlistitem">-->
-        <!--            <a href="music.php?playlist=playlist.txt">playlist.txt</a>-->
-        <!--        </li>-->
     </ul>
 </div>
 </body>
